735c296252d34f182ec56b9ad67abe2d6e717f4f
Adnan-BaOsman <baosman735@gmail.com>
2025-08-12 15:02:46 +0530

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import "./App.css";
 import Body from "./components/Body";
+import { Analytics } from "@vercel/analytics/react";
+import "./components/mobilecss.css";
 
 function App() {
   return (
     <div className="app">
       <Header />
       <Body />
+      <Analytics />
     </div>
   );
 }
