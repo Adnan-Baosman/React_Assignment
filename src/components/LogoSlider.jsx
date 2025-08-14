9e1ce5ea03529dc731f464d9dfda52d05c16a655
Adnan-BaOsman <baosman735@gmail.com>
2025-08-14 06:04:41 +0530

    responsive slider

--- src/components/LogoSlider.jsx
+++ src/components/LogoSlider.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Slider from "react-slick";
 import "./LogoSlider.css";
 import logo1 from "../assets/images/logos/logo1.png";
 import logo2 from "../assets/images/logos/logo2.png";
@@ -9,16 +10,40 @@ import logo5 from "../assets/images/logos/logoq.png";
 const LogoSlider = () => {
   const logos = [logo1, logo2, logo3, logo4, logo5];
 
+  const settings = {
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 4, // visible logos
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false, // hide arrows, optional
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: { slidesToShow: 3 },
+      },
+      {
+        breakpoint: 768,
+        settings: { slidesToShow: 2 },
+      },
+      {
+        breakpoint: 480,
+        settings: { slidesToShow: 1 },
+      },
+    ],
+  };
+
   return (
     <section className="logo-section">
       <p className="sub_heading">Trusted by 100+ Clients Globally</p>
-      <div className="logo-container">
+      <Slider {...settings} className="logo-container">
         {logos.map((logo, idx) => (
           <div key={idx} className="logo-slide">
-            <img src={logo} alt={`Logo ${idx}`} />
+            <img src={logo} alt={`Logo ${idx + 1}`} />
           </div>
         ))}
-      </div>
+      </Slider>
     </section>
   );
 };
--- src/components/headerbkpdup.jsx
+++ src/components/headerbkpdup.jsx
@@ -108,3 +108,31 @@ const Header = () => {
 };
 
 export default Header;
+
+
+import React from "react";
+import "./LogoSlider.css";
+import logo1 from "../assets/images/logos/logo1.png";
+import logo2 from "../assets/images/logos/logo2.png";
+import logo3 from "../assets/images/logos/logo3.png";
+import logo4 from "../assets/images/logos/logo5.png";
+import logo5 from "../assets/images/logos/logoq.png";
+
+const LogoSlider = () => {
+  const logos = [logo1, logo2, logo3, logo4, logo5];
+
+  return (
+    <section className="logo-section">
+      <p className="sub_heading">Trusted by 100+ Clients Globally</p>
+      <div className="logo-container">
+        {logos.map((logo, idx) => (
+          <div key={idx} className="logo-slide">
+            <img src={logo} alt={`Logo ${idx}`} />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default LogoSlider;
